aa9f52ff7d813f6c533de4dd1b51d8d19adf8d58
administrator <burmax98@gmail.com>
2018-11-17 19:43:19 +0200

    віднімання

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -29,9 +29,11 @@ namespace MSROM
     
         public static string Addition(ulong[] a, ulong[] b)
         {
-            var checking = "1328E85EC5F063C3365B199CE8BD083F88D38E795453DBB5B9FC7D7B12CE32236";
+            var checking = "8368BE597855204D694B8AEDD994C2510905AB358E2746B5E4399CE3832EDC8";
             var maxlenght = Math.Max(a.Length, b.Length);
-            var answer = new ulong[maxlenght];
+            var answer = new ulong[maxlenght+1];
+            Array.Resize(ref a, maxlenght);
+            Array.Resize(ref b, maxlenght);
             ulong carry = 0;
            
             for (int i = 0; i < maxlenght; i++)
@@ -52,17 +54,49 @@ namespace MSROM
             return ans;
         }
 
+        public static string Subtraction(ulong[] a, ulong[] b)
+        {
+            var checking = "5DB3F77B1BAEF00A775DCC36FE2FCD4FBCFF03A3F10479FA092073A2688A00C";//
+            var maxlenght = Math.Max(a.Length, b.Length);
+            Array.Resize(ref a, maxlenght);
+            Array.Resize(ref b, maxlenght);
+            var answer = new ulong[maxlenght];
+            ulong borrow = 0;
+            ulong temp = 0;
+            for (int i=0;i<a.Length;i++)
+            {
+                temp=a[i] - b[i] - borrow;
+                if (temp>=0)
+                {
+                    answer[i] = temp;
+                    borrow = 0;
+                }
+                else
+                {
+                    answer[i] = temp & 0xFFFFFFFF;
+                    borrow = 1;
+                }
+            }
+            string ans = string.Concat(answer.Select(chunk => chunk.ToString("X").PadLeft(sizeof(ulong) * 2, '0')).Reverse()).TrimStart('0');
+            if (ans != checking)
+            {
+                Console.WriteLine("VSYO FIGIVO, DYADYA!");
+            }
+            else
+                Console.WriteLine("VSYO NARMALNA");
 
-            static void Main(string[] args)
+            return ans;
+        }
+        static void Main(string[] args)
         { 
-            string a = "D40B81552C3BC8701911D1156C2FF1868C774C2225B073FEBA59AEDED175AB18";
-            string b = "5E83049732CA73C34C9FC8B91FA0927200C19B731F8D475CE56E28D25B6D771E";
+            string a = "708E5AEA4A02082BF054AB926BE247D06302576CBF95E057F6AD0842F5DC6EA";//
+            string b = "12DA636F2E53182178F6DF5B6DB27A80A60353C8CE91665DED8C94A08D526DE";//
             ulong[] p = new ulong[1];
             ulong[] p1 = new ulong[1];
             p1 = toulong(b);
             p = toulong(a);
             Console.WriteLine(Addition(p1, p));
-     
+            Console.WriteLine(Subtraction(p1, p));
             Console.ReadKey();
 
 
